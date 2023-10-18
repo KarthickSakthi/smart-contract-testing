fa797d5333962163432d181647a93d317565b467
Karthick S <karthicksakthi124@gmail.com>
2023-10-18 14:56:51 +0000

    CodeHawks First Flight #1: PasswordStore contract added

---
+++ contracts/PasswordStore.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.4;
+
+//CodeHawks First Flight #1: PasswordStore 
+
+contract PasswordStore {
+    error PasswordStore__NotOwner();
+
+    address private s_owner;
+    string private s_password;
+
+    event SetNetPassword();
+
+    constructor() {
+        s_owner = msg.sender;
+    }
+
+    /*
+     * @notice This function allows only the owner to set a new password.
+     * @param newPassword The new password to set.
+     */
+    function setPassword(string memory newPassword) external {
+        s_password = newPassword;
+        emit SetNetPassword();
+    }
+
+    /*
+     * @notice This allows only the owner to retrieve the password.
+     * @param newPassword The new password to set.
+     */
+    function getPassword() external view returns (string memory) {
+        if (msg.sender != s_owner) {
+            revert PasswordStore__NotOwner();
+        }
+        return s_password;
+    }
+    
+}
