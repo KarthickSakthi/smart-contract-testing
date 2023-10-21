9147bb1e79bf999772926dd39ac45c442ad55114
Karthick S <karthicksakthi124@gmail.com>
2023-10-21 09:05:25 +0000

    CodeHawks FirstFlight: Test

--- test/CodeHawks/PasswordStore.js
+++ test/CodeHawks/PasswordStore.js
@@ -2,29 +2,45 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Password Store Contract should store and return a Password by only owner", async function () {
-  let passwordStorage, signer, password = "CodeHawks@2023";
+  let passwordStorage, signer,signers, password = "CodeHawks@2023";
 
   beforeEach( async function(){
     let PasswordStorage = await ethers.deployContract("PasswordStore");
     passwordStorage = await PasswordStorage.waitForDeployment(6);
-    let  signers = await ethers.getSigners();
+    signers = await ethers.getSigners();
     signer = signers[0]
   })
 
   it("1. Should set a password by an Owner", async function () {
-    const owner = await passwordStorage.s_owner();
-    await passwordStorage.connect(signer);
-    const transaction = await passwordStorage.setPassword(password);
-    await transaction.wait(6);
-    const address = await signer.address;
-    expect(owner).to.equal(address);
-  });
+    const setPasswordTx = await passwordStorage.connect(signer).setPassword(password);
+    await setPasswordTx.wait(6);
+    await expect(setPasswordTx).to.not.be.null;
+    await expect(setPasswordTx).to.emit(passwordStorage, "SetNetPassword");
+});
+
 
   it("2. Should retrieve a Password by Owner", async function () {
-    await passwordStorage.connect(signer);
-    const transaction = await passwordStorage.setPassword(password);
+    const transaction = await passwordStorage.connect(signer).setPassword(password);
     await transaction.wait(6);
-    const currentPassword = await passwordStorage.getPassword();
+    const currentPassword = await passwordStorage.connect(signer).getPassword();
     expect(currentPassword).to.equal(password);
+  });
+
+  it("3. Should not set a password by an other than Owner", async function () {
+    let notOwner = signers[1];
+    await expect(await passwordStorage.connect(notOwner).setPassword(password)).to.be.revertedWithCustomError(
+      passwordStorage,
+      "PasswordStore__NotOwner"
+    );
+  });
+
+
+  it("4. Should not retrieve a Password by other than Owner", async function () {
+    let notOwner = signers[1];
+    await passwordStorage.connect(signer).setPassword(password);
+    await expect(passwordStorage.connect(notOwner).getPassword()).to.be.revertedWithCustomError(
+      passwordStorage,
+      "PasswordStore__NotOwner"
+    );
   });
 });
