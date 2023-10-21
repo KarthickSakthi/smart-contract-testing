50232135f1234d3335f0bea3505e36f628c2bf92
Karthick S <karthicksakthi124@gmail.com>
2023-10-21 09:25:03 +0000

    hardhat config changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
   defaultNetwork: "hardhat",
   networks:{
     mumbai: {
-      url: "", // sepolia RPC endpoint
-      chainId: 80001, // sepolia chain ID
-      accounts: [""],
+      url: process.env.MUMBAI_RPCURL, 
+      chainId: process.env.CHAINID, 
+      accounts: [process.env.PRIVATEKEY1, process.env.PRIVATEKEY2],
     }
   },
 };
